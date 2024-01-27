b14ae7776badb9098704668333cd67be5f2d8d65
Đình Thi <73831236+dinhthi12@users.noreply.github.com>
2024-01-27 22:52:41 +0700

    solution triangle

---
+++ fundamentals/what_triangle.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+//Write a program to input 3 sides of a triangle and tell what triangle it is
+
+void check_triangle(float x, float y, float z){
+
+}
