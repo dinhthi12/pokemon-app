164f883fe3f8e3ad484f057cb9a429dedf0ba482
Đình Thi <73831236+dinhthi12@users.noreply.github.com>
2024-01-30 20:48:24 +0700

    solution for triangle

--- fundamentals/what_triangle.cpp
+++ fundamentals/what_triangle.cpp
@@ -1,9 +1,35 @@
 #include <cmath>
 #include <iostream>
 #include <string>
+using namespace std;
+// Write a program to input 3 sides of a triangle and tell what triangle it is
 
-//Write a program to input 3 sides of a triangle and tell what triangle it is
-
-void check_triangle(float x, float y, float z){
-
+string check_triangle(float a, float b, float c)
+{
+  if (a + b <= c || a + c <= b || b + c <= a)
+  {
+    return "Tam giac khong hop le. Xin kiem tra lai !";
+  }
+  else
+  {
+    if ((a == b) && (b == c))
+    {
+      return "Deu";
+    }
+    else
+    {
+      if (a * a + b * b == c * c || a * c + c * c == b * b || b * b + c * c == a * c)
+      {
+        return "Vuong";
+      }
+      if (a == b || a == c || b == c)
+      {
+        return "Can";
+      }
+      else
+      {
+        return "Thuong";
+      }
+    }
+  }
 }
